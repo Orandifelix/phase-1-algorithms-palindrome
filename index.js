bdb9cddb233685e7182151873bf94cb4ee274993
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-04-19 13:51:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let lowercaseWord = word.toLowerCase();
+  let splitword = lowercaseWord.split("");
+  let reversedarray = splitword.reverse();
+  let reversedword = reversedarray.join("");
+  if (lowercaseWord === reversedword){
+    return true
+
+  }
+  else
+  return false
 }
 
+
 /* 
-  Add your pseudocode here
-*/
+  Add your pseudocode here  
+  word to  lowercase
+  Word to arrange
+  word to reverse 
+  Join the word back 
+  compare with if  and  else 
+
+*/ 
+
 
 /*
   Add written explanation of your solution here
+  change the the word to lowercase or  uppercase.
+  change  the word into an array with .split('')
+  Use the array method .reverse() to reverse the result from above
+  Use  the array method .join("") to  join  the result from above
+  Use if  to compare the final and  then initial input string
+  return true or  false for both cases
 */
 
 // You can run `node index.js` to view these console logs
